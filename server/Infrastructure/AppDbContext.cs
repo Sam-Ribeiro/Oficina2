4bbf7d01a0dcb5ea9fdfaa901ac9808bead558b6
joao marcos <joaomarcosx517@gmail.com>
2026-06-28 20:09:05 -0300

    admin

--- server/Infrastructure/AppDbContext.cs
+++ server/Infrastructure/AppDbContext.cs
@@ -3,22 +3,40 @@ using server.Models;
 
 namespace server.Infrastructure
 {
-    
     public class AppDbContext : DbContext
     {
-        
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
-
         public DbSet<Aluno> Alunos { get; set; }
         public DbSet<Voluntario> Voluntarios { get; set; }
-        public DbSet<Oficina> Oficinas { get; set; } 
+        public DbSet<Oficina> Oficinas { get; set; }
         public DbSet<Aula> Aulas { get; set; }
-        public DbSet<Turma> Turmas { get; set; }       
-        public DbSet<Presenca> Presencas { get; set; } 
+        public DbSet<Turma> Turmas { get; set; }
+        public DbSet<Presenca> Presencas { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Turma>()
+                .HasMany(t => t.Alunos)
+                .WithMany(a => a.Turmas)
+                .UsingEntity(j => j.ToTable("AlunoTurma"));
+
+            
+            modelBuilder.Entity<Voluntario>().HasData(new Voluntario
+            {
+                Id = 1,
+                Nome = "Administrador",
+                
+                CPF = "00000000000",       
+                Email = "admin@admin.com",
+                RA= "123",
+                Senha = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+                Role = "Voluntario"
+            });
+        }
     }
 }
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e31a08719411ba180c7c25c42a7dabc38a83d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d09d47b7f6cc3ac7f3dd95f7d7a24ec559380b")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
